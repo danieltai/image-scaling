9e446123228367d83d74a0e666e75c73be3f747a
Daniel Tai <daniel.tai@rea-group.com>
2024-02-27 19:06:15 +1100

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Daniel Tai on 27/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
